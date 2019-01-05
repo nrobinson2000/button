31b622f3575918ed55f2184cbc0730c2d22dd4e4
Nathan Robinson <nrobinson2000@me.com>
2019-01-04 22:45:33 -0500

    Initial at 22:45 of 2019-01-04

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,11 +1,41 @@
 #include "Particle.h"
 
-void setup() // Put setup code here to run once
-{
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
 
+void startupTasks() {
+        RGB.control(true);
+        RGB.brightness(2);
+        RGB.control(false);
 }
 
-void loop() // Put code here to loop forever
-{
+STARTUP(startupTasks());
 
+const uint32_t tickDebounce = 50 * 1000 * SYSTEM_US_TICKS; // 50ms debounce period
+
+volatile bool ledState;
+
+void blink() {
+        static uint32_t lastTick = 0;
+        if (SYSTEM_TICK_COUNTER - lastTick < tickDebounce) return;
+        lastTick = SYSTEM_TICK_COUNTER;
+        ledState = !ledState;
+
+        digitalWrite(D7, ledState);
+}
+
+
+void setup() {
+        pinMode(A0, INPUT_PULLUP);
+        pinMode(D7, OUTPUT);
+        attachInterrupt(A0, blink, FALLING);
+
+        Serial.begin(115200);
+}
+
+
+
+void loop() {
+        Serial.printlnf("DERP! %ld", millis());
+        delay(5000);
 }
